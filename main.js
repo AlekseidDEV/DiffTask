b9a48d2ac1362ba50fa95f800140b8c48a580754
Alex <lesha.rudiko@gmail.com>
2024-02-11 15:27:19 +0300

    new lesson

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log(1);
+const num = 266219
+
+const arrayMult = String(num).split('').reduce((a, b) => {
+    return +a * +b
+})
+
+const degreeOf = arrayMult ** 3
+
+console.log(String(degreeOf).slice(0, 2));
