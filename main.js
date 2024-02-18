7c0b1a394ca427ea85a606b6d59d9988b04eff89
Alex <lesha.rudiko@gmail.com>
2024-02-18 17:32:38 +0300

    new lesson

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const valueStr = "      54"
+const arr = [285, 475, 541, 420, 652, 201, 800]
 
-const checkStr = (str) => {
-    if (typeof str !== 'string') {
-       return alert('неа, не пойдет')
-    } else if (str.length > 30) {
-        console.log(str.trim().slice(0, 30) + '...');
-    } else{
-        console.log(str.trim());
+arr.forEach((num) => {
+    if (String(num).slice(0, 1) === '2' ||
+        String(num).slice(0, 1) === '4'
+    ){
+        console.log(num);
     }
+})
+
+exitCycle:
+for(let i = 2; i <= 100; i++){
+    for(let f = 2; f < i; f++){
+        if(i % f === 0) continue exitCycle
+    }
+    console.log(`делители этого числа 1 и ${i}`);
 }
 
-checkStr(valueStr)
